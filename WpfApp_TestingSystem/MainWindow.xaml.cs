6c793b1cf49d24565a175638a14f7238a2fe39b7
T500 <mks59k@gmail.com>
2018-08-24 18:03:41 +0300

    Вывод каждого вопроса по порядку.

--- WpfApp_TestingSystem/MainWindow.xaml.cs
+++ WpfApp_TestingSystem/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace WpfApp_TestingSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int numberOfTheCurrentQuestion;
+        private List<Question> questions;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,7 +32,17 @@ namespace WpfApp_TestingSystem
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            this.numberOfTheCurrentQuestion = 0;
+
             this.listBoxAllTests.MouseLeftButtonUp += ListBoxAllTests_MouseLeftButtonUp;
+            this.buttonPassing_Reply.Click += ButtonPassing_Reply_Click;
+        }
+
+        private void ButtonPassing_Reply_Click(object sender, RoutedEventArgs e)
+        {
+            this.numberOfTheCurrentQuestion++;
+
+            this.ShowCurrentQuestion();
         }
 
         private void ListBoxAllTests_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -39,9 +52,59 @@ namespace WpfApp_TestingSystem
 
             this.gridPassingTheTest.Visibility = Visibility.Visible;
 
-            //
+            // Title название теста   // TODO название категории
             this.textBlockPassing_Title.Text
                 = this.listBoxAllTests.SelectedValue.ToString();
+
+            
+
+            using (TestingSystemEntities db = new TestingSystemEntities())
+            {
+                db.Database.Log = Console.Write;
+
+                // TODO загрузить все вопросы -
+                questions = (
+                    from question in db.Question
+                    where question.Test.Name == this.listBoxAllTests.SelectedValue.ToString()
+                    select question
+                    )
+                    .ToList();
+                // TODO загрузить все ответы -
+                //var answers = from answer in db.Answer
+                //                  //join 
+                //              where answer.QuestionId
+
+                // Question
+                //this.textBlockPassing_Question.Text
+                //    = (
+                //    from question in db.Question
+                //    where question.
+                //    select question.QuestionText
+                //    )
+                //    .FirstOrDefault();
+
+
+                // Show
+                // Вывод на экран 1го вопроса
+                this.ShowCurrentQuestion();
+
+                // вывод вопросов
+                //for (int i = 0; i < questions.Count; i++)
+                //{
+                //    MessageBox.Show(questions[i].QuestionText);
+                //}
+            }
+        }
+
+        /// <summary>
+        /// Показать текст текущего вопроса.
+        /// </summary>
+        private void ShowCurrentQuestion()
+        {
+            // проверка на кол-во вопросов
+
+            this.textBlockPassing_Question.Text
+                = questions[this.numberOfTheCurrentQuestion].QuestionText;
         }
 
         private void buttonStudent_Click(object sender, RoutedEventArgs e)
